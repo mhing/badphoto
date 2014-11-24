5b2908b31ae816b740d4619171e422ef5e0d92e1
Matt Hinger <matthew.hinger@gmail.com>
2014-11-23 19:21:15 -0500

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Badphoto::Application.config.secret_key_base = '8a58908257f075e52be3b98a4f22dd9a1b6243cc30cb00aea6a1bf49d10dcc8a98b7c4299f611ca5eaf3e9b6393f16bfe53284c7d6dc025bc0564513135b11c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Badphoto::Application.config.session_store :cookie_store, key: '_badphoto_session'
